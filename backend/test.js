73ff728acab2cff9684618a96525af08d33463d2
Shiva <Karetiss05@gmail.com>
2026-04-21 00:10:18 +0530

    Add files via upload

---
+++ backend/test.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+function testResponse() {
+  const response = "Take a small step and start with 5 minutes.";
+  assert(response.length > 0);
+}
+
+testResponse();
+console.log("Test passed!");
